658c833686288baea405123bd15bd34f98e4c52f
SSSerg05 <ssserg05@gmail.com>
2023-09-06 14:19:12 +0300

    add navlink in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+    // }}
     >
-      React homework template
+      <nav>
+        <NavLink>Home</NavLink>
+        <NavLink>SearchMovies</NavLink>
+        <NavLink>MovieDetails</NavLink>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/movies" element={<div>Movies</div>} />
+        <Route path="/movies/:movieId" element={<div>MovieDetails</div>} />
+        
+          React homework template  
+      </Routes>
+
+      
     </div>
   );
 };
