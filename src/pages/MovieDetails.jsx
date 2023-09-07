cee2f498778c28ab0b3472553a576457cb9047c3
SSSerg05 <ssserg05@gmail.com>
2023-09-07 17:29:51 +0300

    fix

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -2,11 +2,19 @@ import { useParams } from "react-router-dom"
 import { getMovieFromId } from "services/Api";
 
 import { Movie } from "components/MoviesList/Movie/Movie";
+import { useEffect, useState } from "react";
 
 export const MovieDetails = async () => { 
 
-  const params = useParams();
-  const data = await getMovieFromId(params.id);
+  const { movieId } = useParams();
+
+  const [dataMovie, setDataMovie] = useState([]);
+
+  useEffect(async () => { 
+    const data = await getMovieFromId(movieId);  
+  }, [])
+
+  const data = await getMovieFromId(movieId);
   console.log(data);
 
   return (
