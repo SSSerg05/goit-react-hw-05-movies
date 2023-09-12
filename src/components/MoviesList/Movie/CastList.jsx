10274245b9bab7a140e8e0f7af59475c0ef3ed12
SSSerg05 <ssserg05@gmail.com>
2023-09-12 11:36:59 +0300

    fix
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- src/components/MoviesList/Movie/CastList.jsx
+++ src/components/MoviesList/Movie/CastList.jsx
@@ -1,11 +1,11 @@
 import { CastCard } from "./CastCard"
 
 export const CastList = (dataCasts) => {
-  
+
   return ( 
     <ul>
       { dataCasts.map(card => 
-        { return <li key={card.id}><CastCard data={card}></CastCard></li> }) 
+        { return (<li key={card.id}><CastCard data={ card }></CastCard></li>) }) 
       }
     </ul>
   )
