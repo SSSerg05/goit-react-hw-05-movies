32bd3464f554497ce74aed235461251505b663a9
SSSerg05 <ssserg05@gmail.com>
2023-09-19 17:38:50 +0300

    fix

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,62 +1,70 @@
-// import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 // import PropTypes from 'prop-types';
-// import { ToastContainer, toast } from 'react-toastify';
-import { ToastContainer, } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+import { getMoviesFromQuery } from "../../services/Api";
 
 
 import { ImSearch } from 'react-icons/im';
 
-export const Searchbar = () => { 
-// export const Searchbar = ({ onSubmit }) => { 
+//export const Searchbar = () => { 
+export const Searchbar = ({ onSubmit }) => { 
 
   const [searchQuery, setSearchQuery] = useSearchParams();
   const query = searchQuery.get('query') ?? '';
   console.log(query);
 
+  const handleSubmit = event => { 
+    event.preventDefault();
 
+    onSubmit(searchQuery);
+    setSearchQuery({ query: "" });
+  }
 
-  // const handleChangeSearchQuery = event => {
-  //   setSearchQuery( event.currentTarget.value.toLowerCase() )
-  // }
+  const onSearch = async () => { 
+    const query = searchQuery.get('query') ?? '';
+    const controller = new AbortController();
+    
+    try {
+        const data = await getMoviesFromQuery(query, controller);
+        if (data.hits.length === 0) {
+          throw new Error("Gallery empty");
+        }
 
+        console.log('data', data);
 
-  // const handleSubmit = event => { 
-  //   event.preventDefault();
+    } catch (error) {
+        console.log(error.message);
+        // setError(error.message);
+        // onError(error.message);
+    }
+    finally {
+      // setIsLoading(false);
+    }
 
-  //   if (searchQuery.trim() === '') {
-  //     return toast.error('No search query');
-      
-  //   }
-
-  //   onSubmit(searchQuery);
-  //   setSearchQuery("");
-  // }
-
+    setSearchQuery({ query: '' });
+  }
 
   return (
     <div className="Searchbar">
-      <form className="SearchForm" >
-        {/* onSubmit={ handleSubmit }> */}
-          <input
-            className="SearchForm-input"
-            type="text"
-            name="searchQuery"
-            value={ query }
-            onChange={ event => { 
-              searchQuery.set('query', event.target.value.toLowerCase());
-              setSearchQuery(searchQuery);
-            }}
-            placeholder="Search title movie: HW-5 Hook"
-          />
-        <button className="SearchForm-button" type="submit">
-          <ImSearch />
-        </button>
-        <ToastContainer
-          autoClose={2500}
-          theme="colored"/>
-      </form>
+      <input
+        className="SearchForm-input"
+        type="text"
+        name="searchQuery"
+        value={ query }
+        onChange={ event => { 
+          searchQuery.set('query', event.target.value.toLowerCase());
+          setSearchQuery(searchQuery);
+        }}
+        placeholder="Search title movie: HW-5 Hook"
+      />
+      <button onClick={onSearch}>
+        <ImSearch />
+      </button>
+      <ToastContainer
+        autoClose={2500}
+        theme="colored"/>
     </div>
   )
   
--- src/pages/SearchMovies.jsx
+++ src/pages/SearchMovies.jsx
@@ -10,45 +10,47 @@ import { Searchbar } from "components/Searchbar/Searchbar";
 export const SearchMovies = () => { 
 
   const [dataQueryMovies, setDataQueryMovies] = useState([]);
-
+  const [searchQuery, setSearchQuery] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  // useEffect(() => {
-  //   if (!searchQuery) {
-  //     return
-  //   } 
+  useEffect(() => {
+    if (!searchQuery) {
+      return
+    } 
 
-  //   const controller = new AbortController();
-  //   setIsLoading(true);
+    const controller = new AbortController();
+    setIsLoading(true);
 
-  //   async function fetchData() {
-  //     try {
-  //       const data = await getMoviesFromQuery(searchQuery, controller);
-  //       if (data.hits.length === 0) {
-  //         throw new Error("Gallery empty");
-  //       }
+    async function fetchData() {
+      try {
+        const data = await getMoviesFromQuery(searchQuery, controller);
+        if (data.hits.length === 0) {
+          throw new Error("Gallery empty");
+        }
         
-  //       setDataQueryMovies(data)
+        setDataQueryMovies(data)
+        console.log('data', data);
 
-  //     } catch (error) {
-  //       // setError(error.message);
-  //       onError(error.message);
-  //     }
-  //     finally {
-  //       setIsLoading(false);
-  //     }
-  //     return;
-  //   }
+      } catch (error) {
+        console.log(error.message);
+        // setError(error.message);
+        onError(error.message);
+      }
+      finally {
+        setIsLoading(false);
+      }
+      return;
+    }
 
-  //   fetchData();
-  //   return () => {controller.abort()};
-  // }, [searchQuery])
+    fetchData();
+    return () => {controller.abort()};
+  }, [searchQuery])
 
 
 
-  // const handleFormSubmit = searchQuery => {
-  //   setSearchQuery(searchQuery);
-  // }
+  const handleFormSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
+  }
 
 
   // container Toast in component Searchbar
@@ -61,8 +63,7 @@ export const SearchMovies = () => {
   return (
     <div>
       <h1>Search movie</h1>
-      <Searchbar /> 
-      {/* onSubmit={ handleFormSubmit } /> */}
+      <Searchbar onSubmit={ handleFormSubmit } />
 
       {isLoading && <Loader />}
 
